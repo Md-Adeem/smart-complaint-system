4a1a384a72ebdb9ed4de9578580ebf0d74f4295a
Md Adeem <mdadeem2003@gmail.com>
2025-07-16 19:14:41 +0530

    updates

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173","https://localhost:5174"], // frontend origin
+  origin: ["http://localhost:5173","https://localhost:5174","https://smart-complaint-system.vercel.app"], // frontend origin
   credentials: true
 }));
 
--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,20 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
-  withCredentials: true,  // important to send cookies
+  baseURL: import.meta.env.VITE_BASEURL,
+  withCredentials: true, // ✅ required for sending cookies
 });
 
-// REMOVE this interceptor since backend uses cookie auth
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
 export default axiosInstance;
+
