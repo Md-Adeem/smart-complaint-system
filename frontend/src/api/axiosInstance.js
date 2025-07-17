ede9efc8c4f97a2a7c46d0e8edb27ca53256f430
Md Adeem <mdadeem2003@gmail.com>
2025-07-18 01:51:16 +0530

    Prod Bug Fix

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-// Determine environment
-const isProduction = import.meta.env.MODE === "production";
-
-// Use the appropriate base URL
-const baseURL = isProduction 
-  ? import.meta.env.VITE_BASEURL 
-  : import.meta.env.VITE_BASE_URL;
-
 const axiosInstance = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_BASEURL,
   withCredentials: true, // ✅ required for sending cookies
 });
 
 export default axiosInstance;
+
